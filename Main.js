a9a7dc3a37cc40e1e5fdcac93206d36d9812cb19
Verdandi <mxangeltube@gmail.com>
2019-08-18 10:16:53 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         GoGoAnime.io - Costum Style
-// @namespace    https://github.com/Diablosito33/GoGoAnime.Io-CustomStyle
-// @version      1.0.2
-// @description  Made with CSS+JS to alternatively custom web page style
+// @version      1.0.3
 // @author       Diablosito33
+// @name         GoGoAnime.io - Costum Style
+// @description  Made with CSS+JS to alternatively custom web page style
+// @namespace    https://github.com/Diablosito33/GoGoAnime.Io-CustomStyle
 // @homepageURL  https://github.com/Diablosito33/GoGoAnime.Io-CustomStyle
 // @updateURL    https://raw.githubusercontent.com/Diablosito33/GoGoAnime.Io-CustomStyle/master/Main.js
 // @downloadURL  https://raw.githubusercontent.com/Diablosito33/GoGoAnime.Io-CustomStyle/master/Main.js
@@ -14,27 +14,16 @@
 (function() {
     'use strict';
 // 1. Create the button
-var button = document.createElement("button");
-button.innerHTML = "Do Something";
+var btnCiname = document.createElement("button");
+btnCiname.innerHTML = "Do Something";
 
 // 2. Append somewhere
-var flotta = document.getElementsByClassName('favorites_book')[0].parentNode;
-
+var placeAppend = document.getElementsByClassName('favorites_book')[0].parentNode;
+    placeAppend.appendChild(btnCiname);
+    
 // 3. Add event handler
-button.addEventListener ("click", function() {
+button.addEventListener ("click", foo() {
   alert("did something");
 });
 
-var elementA = document.createElement("input");
-    elementA.setAttribute("type", "button");
-    elementA.setAttribute("value", "invert");
-    elementA.setAttribute("name", "button3");
-    elementA.setAttribute("onclick", "foo()");
-    flotta.appendChild(elementA);
-
-/* Read
-YESSSSSSSSSSSSSSSSS
-https://css-tricks.com/use-button-element/
-*/
-    // Your code here...
 })();
